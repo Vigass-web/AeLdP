cac2ae80226b58afa188dccb7dbd73de5636a367
Victor Gabriel <g2171864@gmail.com>
2025-08-01 09:37:47 -0300

    Create Atividade digitos.c

---
+++ Atividades da bimestral/Atividade digitos.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    int num,num2,num3,num4,num5;
+    printf("Informe o primeiro número:");
+    scanf("%d",&num);
+    printf("Informe o segundo número:");
+    scanf("%d",&num2);
+    printf("Informe o terceiro número:");
+    scanf("%d",&num3);
+    printf("Informe o quarto número:");
+    scanf("%d",&num4);
+    printf("Informe o quinto número:");
+    scanf("%d",&num5);
+    printf("%d %d %d %d %d",num,num2,num3,num4,num5);
+    return 0;
+}
