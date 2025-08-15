3a60fee4cc7026aff573828274b5e5d61e987073
Victor Gabriel <g2171864@gmail.com>
2025-08-15 08:55:33 -0300

    Update questao1.c

--- atividades avaliativas em C/questao1.c
+++ atividades avaliativas em C/questao1.c
@@ -6,8 +6,8 @@ int main() {
     printf("Digite um número de 5 dígitos: ");
     scanf("%5s", numero);
 
-    // Imprime os caracteres com 3 espaços entre eles
     printf("%c   %c   %c   %c   %c\n", numero[0], numero[1], numero[2], numero[3], numero[4]);
 
     return 0;
 }
+
